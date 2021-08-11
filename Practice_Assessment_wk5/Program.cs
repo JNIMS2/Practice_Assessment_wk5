3204da7b63a3b80c8021abffb5580a18b8376f2a
Jessica Miller-Nims <Jessicamiller-nims@quickenloans.com>
2021-08-11 15:01:35 -0400

    test1

---
+++ Practice_Assessment_wk5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice_Assessment_wk5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test it out");
+        }
+    }
+}
